75d5df335200091e85ba2532979dab4ada7425e8
Ajaisvraj <ajaisvraj7907@gmail.com>
2023-07-09 22:42:14 +0530

    almost done

--- adminview/urls.py
+++ adminview/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . views import *
 
 urlpatterns = [
+    path('admin_home',admin_home,name='admin_home'),
+    path('registered_users',registered_users,name='registered_users'),
+   
     
 ]
--- adminview/views.py
+++ adminview/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from user.models import UserProfile
+from company.models import CompanyProfile
+from .decorators import admin_login_required
+
 
 # Create your views here.
+
+@admin_login_required
+def admin_home(request):
+    return render(request, 'admin/admin_home.html')
+
+
+
+def registered_users(request):
+    users = UserProfile.objects.all()
+    companies = CompanyProfile.objects.all()
+    context = {
+        'users': users,
+        'companies': companies
+    }
+    return render(request, 'admin/registered_users.html', context)
